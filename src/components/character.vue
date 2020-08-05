21ae94d1a13258663e9b49994a197a3a66548130
David Vivó <david.vivo@oramon.nl>
2020-08-05 13:57:56 +0200

    Create home and charater components

---
+++ src/components/character.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>I'm a SW character!!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+}
+</script>
+
+<style>
+</style>
