19fb29e8d175cb80c869297e239998b8c7679f79
David Vivó <david.vivo@oramon.nl>
2020-08-05 15:41:19 +0200

    Set character initial markup

--- src/components/character.vue
+++ src/components/character.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
+  <article class="swapi-Character_Container">
     <h1>I'm a SW character!!</h1>
-  </div>
+    <h2>Name</h2>
+    <h2>Specie</h2>
+    <h2>Homeworld</h2>
+    <ul>
+      <li>FILM 1</li>
+      <li>FILM 2</li>
+      <li>FILM 3</li>
+    </ul>
+  </article>
 </template>
 
 <script>
@@ -14,4 +22,10 @@ export default {
 </script>
 
 <style>
+  .swapi-Character_Container {
+    padding: 2rem;
+    border: 1px solid #ddd;
+  }
+
+
 </style>
