c641984be2a1742969f7ca8781a3b9212a16d865
David Vivó <david.vivo@oramon.nl>
2020-08-06 17:35:51 +0200

    Display character data

--- src/components/character.vue
+++ src/components/character.vue
@@ -1,13 +1,14 @@
 <template>
   <article class="swapi-Character_Container">
     <h1>I'm a SW character!!</h1>
-    <h2>Name</h2>
-    <h2>Specie</h2>
-    <h2>Homeworld</h2>
+    <h2>My name is {{ name }}</h2>
+    <h2>I'm a {{ species }}</h2>
+    <h2>I'm from {{ homeworld }}</h2>
+    <h2>I'd appeared on this movies:</h2>
     <ul>
-      <li>FILM 1</li>
-      <li>FILM 2</li>
-      <li>FILM 3</li>
+      <li v-for="film in films">
+        {{ film }}
+      </li>
     </ul>
   </article>
 </template>
