d4f37c494f91411f2e06be300241f15933dabbbc
David Vivó <david.vivo@oramon.nl>
2020-08-13 14:46:49 +0200

    Create search component

---
+++ src/components/search.vue
@@ -0,0 +1,33 @@
+<template>
+  <form>
+    <label>Search your character</label>
+    <input type="text" placeholder="Type your search" v-model="searchValue"/>
+    <button type="submit">Submit your search</button>
+  </form>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      searchValue: ''
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+label {
+  display: block;
+  margin-bottom: .75rem;
+}
+
+input {
+  padding: .25rem;
+  max-width: 250px;
+}
+
+</style>
