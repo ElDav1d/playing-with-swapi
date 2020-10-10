c3e6bf84a18dc8345ce1dd2e8fe8d6559bdc973a
David Vivó Palanca <david.vivo@nexum.com>
2020-10-10 17:32:32 +0200

    Create and apply main scss file

--- src/App.vue
+++ src/App.vue
@@ -17,27 +17,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import 'assets/stylesheets/foundation/config.scss';
-
-body {
-  color: $white;
-  background-color: $black;
-}
-
-h1, h2 {
-  color: $yellow;
-  font-weight: normal;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  margin-top: 60px;
-}
-
-a {
-  color: #42b983;
-}
+@import 'assets/stylesheets/styles.scss';
 </style>
