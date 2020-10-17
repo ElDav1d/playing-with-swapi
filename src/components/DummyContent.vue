795cbc7d135a93e0de2c144d433fa7a25c01783e
David Vivó Palanca <david.vivo@nexum.com>
2020-10-17 18:50:37 +0200

    Create and style dummy content component

---
+++ src/components/DummyContent.vue
@@ -0,0 +1,15 @@
+<template>
+  <article class="ContentFrame">
+    <p>
+      Lucas ipsum dolor sit amet grievous cognus tharin kitonak skywalker stereb lyn vaathkree ree-yees thistleborn. Tarpals thakwaash marek hapan r5-d4 wicket elomin mon. Mustafar aayla quadrinaros -1b hutt ranat. Nunb anthos momaw p'w'eck hutt lowbacca terrik verpine cb-99. Kenobi sikan darpa ooryl quence cadavine amidala owen hoth. Darth stass gora bimm droid ferroans jax dooku amidala. Darth carnor sebulba malastare frozian spar. Nal medon hutt rugor thakwaash. Ubese ben amidala lowbacca moff trioculus shaddaa. Evazan umbaran panaka qel-droma rebo er'kit h'nemthean stereb deliah. Leia sly bando chadra-fan yoda ooryl. Var skywalker klivian drach'nam charal. Whill klatooinian iridonian wookiee amedda rattatak spar. Letaki xappyh joh mustafar bith. Max desolous fortuna selonian cornelius celegian ssi-ruuk ord dug. Keshiri ogemite durron ogemite aqualish kurtzen skywalker. Darth zhell grodin beru mace nadon. Thrawn mon kenobi x1. Hissa keshiri thakwaash cerean frozarns paploo yané onimi wicket. Mon bail balosar tavion.
+    </p>
+  </article>
+</template>
+
+<style lang="scss">
+@import '../assets/stylesheets/foundation/helpers.scss';
+
+.ContentFrame {
+  @extend %is-sheet-container;
+}
+</style>
